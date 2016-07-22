b161b404765654e04e948b54368bb930cfa50364
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-22 01:00:34 -0500

    Created Async HTTP Interfaces/Abstracts

---
+++ src/PokemonGoDesktop.Unity.HTTP/Async/IFuture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.Unity.HTTP
+{
+	/// <summary>
+	/// Represents a future (like a task) of the specified
+	/// value and type <paramref name="TFutureType"/>.
+	/// </summary>
+	/// <typeparam name="TFutureType">The resulting future type.</typeparam>
+	public interface IFuture<TFutureType>
+		where TFutureType : class
+	{
+		/// <summary>
+		/// Indicates if the result is available.
+		/// </summary>
+		bool isCompleted { get; }
+
+		TFutureType Result { get; }
+	}
+}
