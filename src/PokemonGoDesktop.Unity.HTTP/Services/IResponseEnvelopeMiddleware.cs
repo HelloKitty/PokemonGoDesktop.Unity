9f8fe824fcdfdfa27ea783381ee81156aa4e9ac4
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-23 05:03:20 -0500

    Add ResponseEnvelope Middleware

---
+++ src/PokemonGoDesktop.Unity.HTTP/Services/IResponseEnvelopeMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Networking.Envelopes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.Unity.HTTP
+{
+	/// <summary>
+	/// Contract for middleware that process a <see cref="ResponseEnvelope"/>.
+	/// </summary>
+	public interface IResponseEnvelopeMiddleware
+	{
+		/// <summary>
+		/// Process logic on the provided <see cref="ResponseEnvelope"/>.
+		/// </summary>
+		/// <param name="envelope">Envelope to process.</param>
+		void ProcessResponseEnvelope(ResponseEnvelope envelope);
+	}
+}
