41d437e39f9678c47893f2b0a8dc7af09f78bb55
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-23 18:14:46 -0500

    Added UserAuthService IoC, Impl, LoginDetails Obsv

---
+++ src/PokemonGoDesktop.Unity.Common/Login/AuthenticationUIObserverDispatcher .cs
@@ -0,0 +1,78 @@
+﻿using PokemonGoDesktop.API.Client.Services;
+using PokemonGoDesktop.API.Common;
+using SceneJect.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PokemonGoDesktop.Unity.Common
+{
+	[Injectee]
+	public class AuthenticationUIObserverDispatcher : MonoBehaviour
+	{
+		/// <summary>
+		/// Indicates the authentication type for this dispatcher.
+		/// </summary>
+		[SerializeField]
+		private AuthType AuthenticationType;
+
+		/// <summary>
+		/// String required for a login/authentication
+		/// (Ex. Username, Email, One-off token)
+		/// </summary>
+		public string LoginString { get; private set; }
+
+		/// <summary>
+		/// Password used for authentication
+		/// </summary>
+		public string Password { get; private set; }
+
+		[Serializable]
+		public class AuthTokenEvent : UnityEvent<IAuthToken> { }
+
+		[SerializeField]
+		private AuthTokenEvent OnValidAuthToken;
+
+		[SerializeField]
+		private AuthTokenEvent OnInvalidAuthToken;
+
+		[Inject]
+		private IUserAuthServiceFactory authServiceFactory;
+
+		//These setters are here for hooking to UnityEvents
+		/// <summary>
+		/// Sets the <see cref="LoginString"/> value.
+		/// Used to hook to <see cref="UnityEvent"/>
+		/// </summary>
+		/// <param name="value">Login string value.</param>
+		public void SetLoginString(string value)
+		{
+			LoginString = value;
+		}
+
+		/// <summary>
+		/// Sets the <see cref="Password"/> value.
+		/// Used to hook to <see cref="UnityEvent"/>
+		/// </summary>
+		/// <param name="value">Password value.</param>
+		public void SetPasswordString(string value)
+		{
+			Password = value;
+		}
+
+		public void TryAuthenticate()
+		{
+			IUserAuthenticationService service = authServiceFactory.Create(AuthenticationType, new AuthenticationDetails(LoginString, Password));
+
+			IAuthToken token = service.TryAuthenticate();
+
+			if (token == null || !token.isValid)
+				OnInvalidAuthToken?.Invoke(token);
+
+			OnValidAuthToken?.Invoke(token);
+		}
+	}
+}
---
+++ src/PokemonGoDesktop.Unity.Common/Login/LoginUrlDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PokemonGoDesktop.Unity.Common
+{
+	[Serializable]
+	public class LoginUrlDetails
+	{
+		[SerializeField]
+		private string _LoginUrl;
+		public string LoginUrl { get { return _LoginUrl; } }
+
+		[SerializeField]
+		private string _OAuthUrl;
+		public string OAuthUrl { get { return OAuthUrl; } }
+	}
+}
