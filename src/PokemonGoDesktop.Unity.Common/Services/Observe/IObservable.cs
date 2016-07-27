1c2e93e818dd4633cd7eb84e38946edd5c9bd0e8
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-27 01:16:30 -0500

    Added net40-ish Observeables

---
+++ src/PokemonGoDesktop.Unity.Common/Services/Observe/IObservable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.Unity.Common
+{
+	//based on net40 IObseravble<T> https://msdn.microsoft.com/en-us/library/dd990377(v=vs.110).aspx
+	public interface IObservable<out T>
+	{
+		/// <summary>
+		/// Subscribers an observer for values of <typeparamref name="T"/>.
+		/// </summary>
+		/// <param name="observer">Observer to recieve values.</param>
+		void Subscribe(IObserver<T> observer);
+	}
+}
---
+++ src/PokemonGoDesktop.Unity.UI/Game/Profile/PlayerDataUIObserver.cs
@@ -0,0 +1,21 @@
+﻿using PokemonGoDesktop.API.Proto;
+using PokemonGoDesktop.Unity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.Unity.UI
+{
+
+
+	public class PlayerDataUIObserver : IObserver<PlayerData>
+	{
+
+
+		public void OnObserved(PlayerData value)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
