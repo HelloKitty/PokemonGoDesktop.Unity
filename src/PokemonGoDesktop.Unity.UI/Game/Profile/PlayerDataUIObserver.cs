6cb8f3c594fbbb307aff26bd5bdbe86bdb43453a
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-27 03:28:49 -0500

    Implemented Player Name on UI

--- src/PokemonGoDesktop.Unity.UI/Game/Profile/PlayerDataUIObserver.cs
+++ src/PokemonGoDesktop.Unity.UI/Game/Profile/PlayerDataUIObserver.cs
@@ -4,18 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
 
 namespace PokemonGoDesktop.Unity.UI
 {
+	[Serializable]
+	public class OnUsernamePlayerDataEvent : UnityEvent<string> { }
 
-
-	public class PlayerDataUIObserver : IObserver<PlayerData>
+	/// <summary>
+	/// Observer (and dispatcher) for <see cref="PlayerData"/>.
+	/// </summary>
+	public class PlayerDataUIObserver : MonoBehaviour, IObserver<PlayerData>
 	{
-
+		/// <summary>
+		/// Invoked when the observer observers a <see cref="PlayerData"/>.
+		/// </summary>
+		[SerializeField]
+		private OnUsernamePlayerDataEvent OnUsernameUpdate;
 
 		public void OnObserved(PlayerData value)
 		{
-			throw new NotImplementedException();
+			OnUsernameUpdate?.Invoke(value.Username);
 		}
 	}
 }
